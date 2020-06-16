5ab55da3f649cf7c1dd37aff4ddcd1944e2b1a9f
Tomas Karlik <karlikt@vscht.cz>
2020-06-16 16:27:19 +0200

    changed indentation fro better readability

--- astar_search.py
+++ astar_search.py
@@ -89,11 +89,11 @@ def _pathIterate(source_node: Node, data_dict: Dict[str, Any]):
         linked_node_pos = link
         link_length = links[link]
         linked_node = work_graph.getNode(linked_node_pos)
-        output_data.append('    Now testing a link to {} of length {}'.format(linked_node, link_length))
+        output_data.append('  Now testing a link to {} of length {}'.format(linked_node, link_length))
 
         # B) If the linked node has already been visited in this path, skip this link and continue with the next
         if linked_node in active_paths[source_path_id]:
-            output_data.append("        but it has already been visited in this path and thus will be skipped")
+            output_data.append("    but it has already been visited in this path and thus will be skipped")
             continue
 
         # C) Get the complete path from start to the linked node and its weight
@@ -102,7 +102,7 @@ def _pathIterate(source_node: Node, data_dict: Dict[str, Any]):
         # D) If the new path's length matches or exceeds the length of the current best path to the goal, skip this link
         new_best_length = data_dict['best_path'].length
         if new_path.length >= new_best_length:
-            output_data.append("        This path's length is longer than the best path to the goal ({} >= {}) and it"
+            output_data.append("    This path's length is longer than the best path to the goal ({} >= {}) and it"
                                " will be skipped".format(new_path.length, new_best_length))
             continue
 
@@ -123,7 +123,7 @@ def _pathIterate(source_node: Node, data_dict: Dict[str, Any]):
         # G) Finally, add the new path to active paths
         index = len(active_paths)
         active_paths[index] = new_path
-        output_data.append("        {} has been added to the active paths".format(new_path))
+        output_data.append("    {} has been added to the active paths".format(new_path))
 
     # 3) Disable the fully expanded path
     output_data.append("{} has been fully expanded and thus it has been disabled".format(
@@ -155,7 +155,7 @@ def _pathIterate(source_node: Node, data_dict: Dict[str, Any]):
 
 def goalNodeUpdate(new_path: Path, data_dict: Dict[str, Any]) -> Path:
     """Update the best goal path and disable all active paths that are longer than the new best goal path."""
-    data_dict['output_data'].append("        AND IT IS THE GOAL!")
+    data_dict['output_data'].append("    AND IT IS THE GOAL!")
     active_paths = data_dict['active_paths']
     # Update the best goal path
     data_dict['best_path'] = new_path
@@ -164,12 +164,12 @@ def goalNodeUpdate(new_path: Path, data_dict: Dict[str, Any]) -> Path:
     for path_id in [path_id for path_id in active_paths if active_paths[path_id].enabled]:
         path_length = active_paths[path_id].length
         if path_length > new_best_length:
-            data_dict['output_data'].appendinfo("        {} has been disabled because it's longer than the new"
-                                                " best path to the goal ({} > {})".format(active_paths[path_id],
-                                                                                          path_length, new_best_length))
+            data_dict['output_data'].append("    {} has been disabled because it's longer than the new"
+                                            " best path to the goal ({} > {})".format(active_paths[path_id],
+                                                                                      path_length, new_best_length))
             active_paths[path_id].enabled = False
     # Disable the new goal path to avoid expanding it in future iterations
-    data_dict['output_data'].append("        the new path to the goal has been disabled to avoid "
+    data_dict['output_data'].append("    the new path to the goal has been disabled to avoid "
                                     "expanding it in future iterations")
     new_path.enabled = False
     return new_path
@@ -180,11 +180,11 @@ def sameNodeCompare(best_length: Int_or_Float, new_path: Path, path_id: int, act
     """Compare the path with the same ending node to the current shortest path to that node and pick the shorter."""
     path_length = active_paths[path_id].length
     if path_length > best_length:
-        data_dict['output_data'].append("        {} has been disabled because this path gets to "
+        data_dict['output_data'].append("    {} has been disabled because this path gets to "
                                         "the {} node faster".format(active_paths[path_id], linked_node.name))
         active_paths[path_id].enabled = False
     else:
-        data_dict['output_data'].append("        this path has been disabled because {} gets to "
+        data_dict['output_data'].append("    this path has been disabled because {} gets to "
                                         "the {} node faster".format(active_paths[path_id], linked_node.name))
         new_path.enabled = False
         best_length = path_length
